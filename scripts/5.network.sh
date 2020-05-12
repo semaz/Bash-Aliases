24377173a300ba686ce05721482a73d2913199b7
Creel <devcreel@gmail.com>
2020-05-13 00:01:40 +0300

    remove useless scripts

--- scripts/5.network.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-alias flushDNS='dscacheutil -flushcache && killall -HUP mDNSResponder'
-alias myip="curl http://ipecho.net/plain; echo"
-alias trace='mtr --report-wide --curses $1'
-alias openPorts='lsof -i | grep LISTEN'
-alias edithosts='sudo nano /etc/hosts'
